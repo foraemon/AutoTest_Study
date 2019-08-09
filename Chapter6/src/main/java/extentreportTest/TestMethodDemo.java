918564285690a45af68e9587ad027281b37db1b2
foraemon <dzl_player@163.com>
2019-08-09 17:51:35 +0800

    ExtentReports的使用

---
+++ Chapter6/src/main/java/extentreportTest/TestMethodDemo.java
@@ -0,0 +1,31 @@
+package extentreportTest;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import javax.security.auth.login.AccountExpiredException;
+
+/**
+ * @author zoro
+ * @date 2019/8/9 0009 下午 3:18
+ */
+public class TestMethodDemo {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","bbb");
+    }
+    @Test
+    public void logDemo(){
+        Reporter.log("这是我们自己写的日志");
+        throw new RuntimeException("这是我自己的运行异常");
+    }
+}
